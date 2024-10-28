d8f5a4f3420b550cfb5c4624bf1a29aa889ca49b
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-28 15:37:36 +0300

    Company model finished.

--- src/main/java/com/yildiz/expenly/model/Company.java
+++ src/main/java/com/yildiz/expenly/model/Company.java
@@ -1,4 +1,29 @@
 package com.yildiz.expenly.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String companyName;
+    private String companyAddress;
+    private String companyLogoUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "company")
+    private List<Expense> expenses;
+
 }
