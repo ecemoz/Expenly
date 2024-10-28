38d2c0262639e115b5380f726bf343196631a623
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-28 17:22:20 +0300

    Expense service finished.

--- src/main/java/com/yildiz/expenly/service/ExpenseService.java
+++ src/main/java/com/yildiz/expenly/service/ExpenseService.java
@@ -1,4 +1,40 @@
 package com.yildiz.expenly.service;
 
+import com.yildiz.expenly.model.Expense;
+import com.yildiz.expenly.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ExpenseService {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    public Expense createExpense(Expense expense){
+        return expenseRepository.save(expense);
+    }
+
+    public List<Expense> getAllExpenses(){
+        return expenseRepository.findAll();
+    }
+
+    public Optional<Expense> getExpenseById(Long id){
+        return expenseRepository.findById(id);
+    }
+
+    public void deleteExpenseById(Long id){
+        if (expenseRepository.existsById(id)) {
+            expenseRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Expense not found");
+        }
+    }
+
+    public List<Expense> getExpensesByCompanyId(Long companyId){
+        return expenseRepository.findByCompany_Id(companyId);
+    }
 }
