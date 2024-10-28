019b6e94814034b56d708977ad288001fccea8ba
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-28 15:18:28 +0300

    User model finished

--- src/main/java/com/yildiz/expenly/model/User.java
+++ src/main/java/com/yildiz/expenly/model/User.java
@@ -1,4 +1,24 @@
 package com.yildiz.expenly.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List <Company> companies;
 }
