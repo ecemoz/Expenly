5f1b95a69328bcc05d7eb42835c2d7205f335a43
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-28 17:01:02 +0300

    user service finished.

--- src/main/java/com/yildiz/expenly/service/UserService.java
+++ src/main/java/com/yildiz/expenly/service/UserService.java
@@ -1,4 +1,47 @@
 package com.yildiz.expenly.service;
 
+import com.yildiz.expenly.model.User;
+import com.yildiz.expenly.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id)
+                .map(existingUser -> {
+                    existingUser.setUsername(updatedUser.getUsername());
+                    existingUser.setPassword(updatedUser.getPassword());
+                    existingUser.setEmail(updatedUser.getEmail());
+                    return userRepository.save(existingUser);
+                })
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
