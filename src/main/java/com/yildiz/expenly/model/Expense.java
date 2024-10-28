5f979f36eb63a12fe1b8644ec1b8128182d926b5
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-28 16:03:34 +0300

    expense model finished

--- src/main/java/com/yildiz/expenly/model/Expense.java
+++ src/main/java/com/yildiz/expenly/model/Expense.java
@@ -1,4 +1,31 @@
 package com.yildiz.expenly.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String storeName;
+    private LocalDate expenseDate;
+    private LocalTime expenseTime;
+    private BigDecimal totalExpense;
+    private BigDecimal taxAmount;
+    private BigDecimal taxlessExpense;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+    
 }
