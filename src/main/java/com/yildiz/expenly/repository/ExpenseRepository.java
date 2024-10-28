1236050aa715fd96569680d2c6c0c5b6478ecd7a
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-28 16:32:07 +0300

    Expense Repository finished.

--- src/main/java/com/yildiz/expenly/repository/ExpenseRepository.java
+++ src/main/java/com/yildiz/expenly/repository/ExpenseRepository.java
@@ -1,4 +1,10 @@
 package com.yildiz.expenly.repository;
 
-public class ExpenseRepository {
+import com.yildiz.expenly.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByCompany_Id(Long companyId);
 }
