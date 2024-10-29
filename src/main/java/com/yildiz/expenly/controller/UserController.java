d31ce409c598d29c83256f5d0addfba90beb50ad
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-29 15:21:17 +0300

    User controller finished.

--- src/main/java/com/yildiz/expenly/controller/UserController.java
+++ src/main/java/com/yildiz/expenly/controller/UserController.java
@@ -1,4 +1,58 @@
 package com.yildiz.expenly.controller;
 
+
+import com.yildiz.expenly.model.User;
+import com.yildiz.expenly.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity <User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername (@PathVariable String username){
+        Optional<User> user = userService.getUserByUsername(username);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}")
+    public  ResponseEntity<User> updateUser(@PathVariable  Long id , @RequestBody User updatedUser) {
+        try {
+            User user = userService.updateUser(id, updatedUser);
+            return ResponseEntity.ok(user);
+        } catch (RuntimeException e) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable  Long id) {
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return  ResponseEntity.notFound().build();
+        }
+    }
 }
