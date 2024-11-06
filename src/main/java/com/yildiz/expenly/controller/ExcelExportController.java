071e02e4d4d3d981c6a971d5580df4cbd62d7349
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-06 17:52:40 +0300

    Create ExcelExport Controller

---
+++ src/main/java/com/yildiz/expenly/controller/ExcelExportController.java
@@ -0,0 +1,44 @@
+package com.yildiz.expenly.controller;
+
+import com.yildiz.expenly.utils.ExcelExportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/export")
+public class ExcelExportController {
+
+    @Autowired
+    private ExcelExportService excelExportService;
+
+    @GetMapping("/expenses/{companyId}")
+    public ResponseEntity<byte[]> exportExpenses(@PathVariable Long companyId) throws IOException {
+        String filePath = System.getProperty("java.io.tmpdir") + "expenses.xlsx";
+
+        excelExportService.exportExpensesToExcel(companyId, filePath);
+
+        File file = new File(filePath);
+        byte[] fileContent;
+        try(FileInputStream fis = new FileInputStream(file);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream()){
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                bos.write(buffer, 0, bytesRead);
+            }
+            fileContent = bos.toByteArray();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=expenses.xlsx")
+                .body(fileContent);
+    }
+}
